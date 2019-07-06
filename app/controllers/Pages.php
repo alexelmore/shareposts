d253de046a01a9574a1531e499cabc2fb98be464
Alex Elmore <alexelmore49@gmail.com>
2019-07-06 12:28:14 -0500

    The start of the sharepost application

---
+++ app/controllers/Pages.php
@@ -0,0 +1,24 @@
+<?php
+
+class Pages extends Controller
+{
+    public function __construct()
+    { }
+
+    public function index()
+    {
+
+        $data = [
+            'title' => 'ElmoreMVC',
+
+        ];
+
+        $this->view('pages/index', $data);
+    }
+
+    public function about()
+    {
+        $data = ['title' => 'about'];
+        $this->view('pages/about', $data);
+    }
+}
