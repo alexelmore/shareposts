e88b2992326be90b721d8e2ed2c933e3f7ced7ac
Alex Elmore <alexelmore49@gmail.com>
2019-07-06 14:19:23 -0500

    Registration and login forms created

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -27,4 +27,24 @@ class Users extends Controller
             $this->view('users/register', $data);
         }
     }
+
+
+    public function login()
+    {
+        // Check for posts
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            //Process Form
+        } else {
+            // Init data
+            $data = [
+                'email' => '',
+                'password' => '',
+                'email_err' => '',
+                'password_err' => '',
+            ];
+
+            // Load view
+            $this->view('users/login', $data);
+        }
+    }
 }
