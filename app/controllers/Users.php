92d65e992c116473f508c03fbcb44c43abe37280
Alex Elmore <alexelmore49@gmail.com>
2019-07-08 10:41:46 -0500

    Added session and flash message for user registration.

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -71,6 +71,7 @@ class Users extends Controller
 
                 // Register User
                 if ($this->userModel->register($data)) {
+                    flash('register_success', 'You are registered and can now login');
                     redirect('users/login');
                 } else {
                     die('Something went wrong');
